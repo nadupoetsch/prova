795888c036f40fad12ad055875e6d11ec05c4991
Bernardo Poetsch <89049723+nadupoetsch@users.noreply.github.com>
2022-03-23 03:01:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*alert('inicio')
+
+for(var i=10;i<0;i--){
+  alert('passei: ' + i)
+}
+
+alert('fim')
+*/
+
+var vetor = [12,50,10,35,40]
+
+for(var i=0;i<vetor.length;i++){
+  console.log(vetor[4-i])
+}
